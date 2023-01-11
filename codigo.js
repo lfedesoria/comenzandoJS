a09896eac1650a27fd37198d544b71df1bab3b25
Federico <lfede.soria@gmail.com>
2023-01-10 21:23:55 -0300

    tipos de datos, operadores, funcion basica

---
+++ codigo.js
@@ -0,0 +1,36 @@
+// Variables (cajas donde ponemos datos)
+
+// var nombre = "Juan";
+// console.log(nombre);
+
+// TIPOS DE DATOS 
+
+// string = "Hola"; // los string siempre van entre comillas
+// number = 1 // todos los numeros no van entre comillas
+// booleanos = true || false 
+// objetos  
+// array 
+// null 
+// undefined 
+// NaN 
+
+// OPERADORES
+
+// = asignacion 
+// == comparacion 
+// === comparacion estricta (igualdad de tipo y valor)
+// != diferente 
+// !== estrictamente diferente 
+// > mayor que 
+// < menor que
+// >= mayor o igual que 
+// <= menor o igual que 
+// || or 
+// && and 
+
+function sumar(a,b){
+    return a + b;
+}
+console.log(sumar(1,2));
+
+
