fd51257dd9a4e6635486bf1025e84830206a3a65
Federico <lfede.soria@gmail.com>
2023-01-18 11:48:06 -0300

    resuelvo algunos ejercicios

--- ejercicios.js
+++ ejercicios.js
@@ -1,3 +1,5 @@
+// EJERCICIOS BASICOS !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
 // 01
 // alert('un mensaje')
 
@@ -80,7 +82,7 @@ if (condicion verdadera o falsa) {
 //     case "u":
 //       document.write(frase.substring(i, i+1) + "<br>");
 //       break;
-//     default: 
+//     default:
 //      console.log('no es una vocal');
 //   }
 // }
@@ -117,20 +119,120 @@ if (condicion verdadera o falsa) {
 //   document.write("El numero no es divisible por 2, 3, 5 o 7");
 // }
 
-// EJERCICIOS BUCLES
+// EJERCICIOS BUCLES !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+// 01
+
+// let edad = +prompt('ingrese su edad')
+
+// if (edad >18 && edad<98) {
+//   alert('Apto para conducir')
+// } if (edad < 18 || edad > 98) {
+//   alert('No apto para conducir')
+// } else {
+//   alert ('ingrese un numero valido')
+// }
+
+//02
+
+// do {
+//   let nota = prompt("Introduce tu nota");
+//   //SI SE INTRODUJO UN NÚMERO
+//   if (Number(nota) === nota) {
+//     //SI LA NOTA ES ENTRE 0 Y 10 COMPRUEBA EL RANGO Y DA UN MENSAJE
+//     if (nota >= 0 && nota <= 10) {
+//       if (nota >= 0 && nota <= 2) {
+//         alert("Muy deficiente");
+//       } else if (nota >= 3 && nota <= 4) {
+//         alert("Insuficiente");
+//       } else if (nota >= 5 && nota <= 6) {
+//         alert("Suficiente");
+//       } else if ((nota = 7)) {
+//         alert("Bien");
+//       } else if (nota >= 8 && nota <= 9) {
+//         alert("Notable");
+//         5;
+//       } else if ((nota = 10)) {
+//         alert("Sobresaliente");
+//       }
+//     }
+//     //SI LA NOTA NO ES ENTRE 0 Y 10
+//     else {
+//       alert("Nota erronea");
+//     }
+//   }
+//   //SI LA NOTA INTRODUCIDA NO ES UN NÚMERO
+//   else {
+//     //SI SE HA PULSADO ACEPTAR SIN INTRODUCIR NADA
+//     if (nota != undefined) {
+//       //No es Undefined cuando se pulsa aceptar.
+//       alert("Introduce un numero valido");
+//     }
+//   }
+//   //EL BUCLE VUELVE ARRIBA MIENTRAS NO SE HAYA PULSADO CANCELAR
+// } while (nota != undefined); //Undefined es cuando se pulsa Cancelar. SE REALIZA EL DO MIENTRAS NOTA NO SEA UNDEFINED
 
 // 03
 /* Realiza un script que pida cadenas de texto  hasta que se pulse “cancelar”. 
 Al salir con “cancelar” deben mostrarse todas las cadenas concatenadas con un guión -. */
 
-// let frase = ''
-// let fraseNueva = ''
+let frase = ''
+let fraseNueva = ''
 
-// while (confirm('desea ingresar texto?') === true) {
-//   frase = prompt('ingrese texto');
-//   if (frase !== null) {
-//     fraseNueva = fraseNueva + frase + '-';
-//   }
-// }
+while (confirm('desea ingresar texto?') === true) {
+  frase = prompt('ingrese texto');
+  if (frase !== null) {
+    fraseNueva = fraseNueva + frase + '-';
+  }
+}
 
-// document.write(fraseNueva);
+document.write(fraseNueva);
+
+// 04 
+
+do {
+  let nota = prompt("Introduce tu nota");
+  
+  if (Number(nota) === nota) {
+    //SI LA NOTA ES ENTRE 0 Y 10 COMPRUEBA EL RANGO Y DA UN MENSAJE
+    if (nota >= 0 && nota <= 10) {
+      if (nota >= 0 && nota <= 2) {
+        alert("Muy deficiente");
+      } else if (nota >= 3 && nota <= 4) {
+        alert("Insuficiente");
+      } else if (nota >= 5 && nota <= 6) {
+        alert("Suficiente");
+      } else if ((nota = 7)) {
+        alert("Bien");
+      } else if (nota >= 8 && nota <= 9) {
+        alert("Notable");
+        5;
+      } else if ((nota = 10)) {
+        alert("Sobresaliente");
+      }
+    }
+    //SI LA NOTA NO ES ENTRE 0 Y 10
+    else {
+      alert("Nota erronea");
+    }
+  }
+  //SI LA NOTA INTRODUCIDA NO ES UN NÚMERO
+  else {
+    //SI SE HA PULSADO ACEPTAR SIN INTRODUCIR NADA
+    if (nota != undefined) {
+      //No es Undefined cuando se pulsa aceptar.
+      alert("Introduce un numero valido");
+    }
+  }
+  //EL BUCLE VUELVE ARRIBA MIENTRAS NO SE HAYA PULSADO CANCELAR
+} while (nota != undefined); //Undefined es cuando se pulsa Cancelar. SE REALIZA EL DO MIENTRAS NOTA NO SEA UNDEFINED
+
+let numero = +prompt ('ingrese un numero')
+let cadenaNumero = ''
+
+while (numero !== ) {
+  
+}
+
+
+
